f9f4dd583673688d820635356c4b9b8525b67bc2
stefanoreddd <stefanogg95@gmail.com>
2023-04-20 18:14:57 +0200

    Class "Cibo in scatola" completed.

--- C# Shop 2/ClassSubCiboInScatola.cs
+++ C# Shop 2/ClassSubCiboInScatola.cs
@@ -4,9 +4,96 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace C__Shop_2
+namespace Negozio
 {
-    internal class ClassSubCiboInScatola
+    public class CiboInScatola : Prodotti
     {
+
+        //ATTRIBUTI
+
+        private int peso;
+        private int marca;
+
+
+        //COSTRUTTORE
+
+        public CiboInScatola(string productName, string productCategory, string productDescription, int productPrice, int peso, int marca) : base(productName, productCategory, productDescription, productPrice)
+        {
+            this.peso = peso;
+            this.marca = marca;
+        }
+
+
+        //GETTERS
+
+        public int GetPeso()
+        {
+            return this.peso;
+        }
+
+        public int GetMarca()
+        {
+            return this.marca;
+        }
+
+
+        //SETTERS
+
+        public void SetPeso(int peso)
+        {
+            if (this.peso > 300)
+            {
+                Console.WriteLine("Non puoi inserire più di 300g di cibo!");
+            }
+            else if (this.peso <= 300) 
+            {
+                Console.WriteLine("Peso modificato!");
+            }
+            this.peso = peso;
+        }
+
+
+        //METHODS
+
+        public int MangiaCibo()
+        {
+            int ciboDaMangiare = 1;
+            Console.WriteLine("Chomp chomp...");
+            this.peso = peso - ciboDaMangiare;
+            Thread.Sleep(5000);
+            Console.WriteLine("Peso cibo dopo aver mangiato: " + this.peso);
+            return this.peso;
+        }
+
+        public float SvuotaScatola()
+        {
+            Console.WriteLine("*Svuota la scatola*");
+            Thread.Sleep(5000);
+            Console.WriteLine("Scatola vuoto!");
+            this.peso = 0;
+            return this.peso;
+        }
+
+        public int RiempiScatola()
+        {
+            int ciboDaInserire = 0;
+
+            Console.WriteLine("Quanto cibo (in grammi) vuoi inserire?");
+            ciboDaInserire = int.Parse(Console.ReadLine());
+            Console.WriteLine("*Riempie la bottiglia*");
+            int ciboFinale = this.peso + ciboDaInserire;
+            Thread.Sleep(5000);
+            if (ciboFinale > this.peso)
+            {
+                Console.WriteLine("Hai inserito troppo cibo!");
+            }
+            else if (ciboFinale == this.peso)
+            {
+                Console.WriteLine("Sacchetto riempito!");
+            }
+
+            this.peso = ciboFinale;
+            return this.peso;
+        }
     }
 }
--- C# Shop 2/ClassSubElettrodomestico.cs
+++ C# Shop 2/ClassSubElettrodomestico.cs
@@ -4,9 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace C__Shop_2
+namespace Negozio
 {
-    internal class ClassSubElettrodomestico
+    public class Elettrodomestico : Prodotti
     {
+
+        //ATTRIBUTI
+
+        private string marca;
+        private int peso;
+        private string anno;
+
+
+        //COSTRUTTORE
+        public Elettrodomestico(string productName, string productCategory, string productDescription, int productPrice, string marca, int peso, string anno) : base(productName, productCategory, productDescription, productPrice)
+        {
+            this.marca = marca;
+            this.peso = peso;
+            this.anno = anno;
+        }
+
+
+        //GETTERS
+
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+
+        public int GetPeso()
+        {
+            return this.peso;
+        }
+
+        public string GetAnno()
+        {
+            return this.anno;
+        }
+
+
+        //SETTERS
+
+        public void SetMarca(string marca)
+        {
+            this.marca = marca;
+        }
     }
+
 }
